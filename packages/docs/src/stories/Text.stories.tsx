bb93d3a65f25ffe0783cd81cbc92f62d7e7fac40
Luis Otavio Longo <luisptavio.l.p@gmail.com>
2022-11-25 11:48:09 -0300

    feat: Add core components

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,43 @@
+import { Text, TextProps } from '@ignite-ui/react'
+import { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    size: 'md',
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo dignissimos veniam rerum repellendus ut. Laboriosam expedita, eos nobis a aut similique illo ipsa incidunt, nesciunt id iusto. Nihil, architecto perspiciatis!',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong Text',
+    as: 'strong',
+  },
+}
